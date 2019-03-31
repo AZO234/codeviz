a98c7cb19956b27730b5d7641109fdaf2697fe58
AZO234 <sylph23k@gmail.com>
2019-03-31 22:52:20 +0900

    apply to GCC 8.3.0

--- compilers/install_gcc-8.3.0.sh
+++ compilers/install_gcc-8.3.0.sh
@@ -5,17 +5,17 @@ if [ "$1" != "" ]; then INSTALL_PATH=$1; fi
 if [ "$2" = "compile-only" ]; then export COMPILE_ONLY=yes; fi
 echo Installing gcc to $INSTALL_PATH
 
-NCFTP=`which ncftpget`
+NCFTP=`which wget`
 EXIT=$?
 if [ "$EXIT" != "0" ]; then
   NCFTP=ftp
 fi
 
-if [ ! -e gcc-4.6.2.tar.gz ]; then
-  echo gcc-4.6.2.tar.gz not found, downloading
-  $NCFTP ftp://ftp.gnu.org/pub/gnu/gcc/gcc-4.6.2/gcc-4.6.2.tar.gz
-  if [ ! -e gcc-4.6.2.tar.gz ]; then
-    echo Failed to download gcc, download gcc-4.6.2.tar.gz from www.gnu.org
+if [ ! -e gcc-8.3.0.tar.xz ]; then
+  echo gcc-8.3.0.tar.xz not found, downloading
+  $NCFTP ftp://ftp.gnu.org/pub/gnu/gcc/gcc-8.3.0/gcc-8.3.0.tar.xz
+  if [ ! -e gcc-8.3.0.tar.xz ]; then
+    echo Failed to download gcc, download gcc-8.3.0.tar.xz from www.gnu.org
     exit
   fi
 fi
@@ -24,26 +24,25 @@ fi
 rm -rf gcc-graph/objdir 2> /dev/null
 mkdir -p gcc-graph/objdir
 echo Untarring gcc...
-tar -zxf gcc-4.6.2.tar.gz -C gcc-graph || exit
+tar -Jxf gcc-8.3.0.tar.xz -C gcc-graph || exit
 
 # Apply patch
-cd gcc-graph/gcc-4.6.2
-patch -p1 < ../../gcc-patches/gcc-4.6.2-codeviz.diff
-patch -p1 < ../../gcc-patches/gcc-4.6.2-cdepn.diff
+cd gcc-graph/gcc-8.3.0
+patch -p1 < ../../gcc-patches/gcc-8.3.0-cdepn.diff
 cd ../objdir
 
 # Configure and compile
-../gcc-4.6.2/configure --prefix=$INSTALL_PATH --enable-shared --enable-languages=c,c++ || exit
+../gcc-8.3.0/configure --prefix=$INSTALL_PATH --enable-shared --enable-languages=c,c++ || exit
 make bootstrap
 
 RETVAL=$?
-PLATFORM=i686-pc-linux-gnu
+PLATFORM=x86_64-pc-linux-gnu
 if [ $RETVAL != 0 ]; then
   if [ ! -e $PLATFORM/libiberty/config.h ]; then
     echo Checking if this is CygWin
     echo Note: This is untested, if building with Cygwin works, please email mel@csn.ul.ie with
     echo a report
-    export PLATFORM=i686-pc-cygwin
+    export PLATFORM=x86_64-pc-cygwin
     if [ ! -e $PLATFORM/libiberty/config.h ]; then
       echo Do not know how to fix this compile error up, exiting...
       exit -1
@@ -54,7 +53,7 @@ if [ $RETVAL != 0 ]; then
   cat config.h | sed -e 's/.*undef HAVE_STDLIB_H.*/\#define HAVE_STDLIB_H 1/' > config.h.tmp && mv config.h.tmp config.h
   cat config.h | sed -e 's/.*undef HAVE_UNISTD_H.*/\#define HAVE_UNISTD_H 1/' > config.h.tmp && mv config.h.tmp config.h
   cat config.h | sed -e 's/.*undef HAVE_SYS_STAT_H.*/\#define HAVE_LIMITS_H 1/' > config.h.tmp && mv config.h.tmp config.h
-  if [ "$PLATFORM" = "i686-pc-cygwin" ]; then
+  if [ "$PLATFORM" = "x86_64-pc-cygwin" ]; then
     echo "#undef HAVE_GETTIMEOFDAY" >> config.h
   fi
 
