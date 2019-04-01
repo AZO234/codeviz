d039e0f27ecb8a21918d6a81fa9c53de5b981981
AZO234 <sylph23k@gmail.com>
2019-04-01 12:17:08 +0900

    Cygwin AArch64 multithread-build

--- compilers/install_gcc-8.3.0.sh
+++ compilers/install_gcc-8.3.0.sh
@@ -5,15 +5,17 @@ if [ "$1" != "" ]; then INSTALL_PATH=$1; fi
 if [ "$2" = "compile-only" ]; then export COMPILE_ONLY=yes; fi
 echo Installing gcc to $INSTALL_PATH
 
-NCFTP=`which wget`
+JOBS=`which nproc`
 EXIT=$?
 if [ "$EXIT" != "0" ]; then
-  NCFTP=ftp
+  JOBS=1
+else
+  JOBS=`nproc`
 fi
 
 if [ ! -e gcc-8.3.0.tar.xz ]; then
   echo gcc-8.3.0.tar.xz not found, downloading
-  $NCFTP ftp://ftp.gnu.org/pub/gnu/gcc/gcc-8.3.0/gcc-8.3.0.tar.xz
+  wget ftp://ftp.gnu.org/pub/gnu/gcc/gcc-8.3.0/gcc-8.3.0.tar.xz
   if [ ! -e gcc-8.3.0.tar.xz ]; then
     echo Failed to download gcc, download gcc-8.3.0.tar.xz from www.gnu.org
     exit
@@ -33,19 +35,25 @@ cd ../objdir
 
 # Configure and compile
 ../gcc-8.3.0/configure --prefix=$INSTALL_PATH --enable-shared --enable-languages=c,c++ || exit
-make bootstrap
+make bootstrap -j$JOBS
 
 RETVAL=$?
 PLATFORM=x86_64-pc-linux-gnu
 if [ $RETVAL != 0 ]; then
   if [ ! -e $PLATFORM/libiberty/config.h ]; then
-    echo Checking if this is CygWin
-    echo Note: This is untested, if building with Cygwin works, please email mel@csn.ul.ie with
+    echo Checking if this is AArch64
+    echo Note: This is untested, if building with AArch64 works, please email mel@csn.ul.ie with
     echo a report
-    export PLATFORM=x86_64-pc-cygwin
+    PLATFORM=aarch64-linux-gnu
     if [ ! -e $PLATFORM/libiberty/config.h ]; then
-      echo Do not know how to fix this compile error up, exiting...
-      exit -1
+      echo Checking if this is CygWin
+      echo Note: This is untested, if building with Cygwin works, please email mel@csn.ul.ie with
+      echo a report
+      export PLATFORM=x86_64-unknown-cygwin
+      if [ ! -e $PLATFORM/libiberty/config.h ]; then
+        echo Do not know how to fix this compile error up, exiting...
+        exit -1
+      fi
     fi
   fi
   cd $PLATFORM/libiberty/
@@ -53,7 +61,7 @@ if [ $RETVAL != 0 ]; then
   cat config.h | sed -e 's/.*undef HAVE_STDLIB_H.*/\#define HAVE_STDLIB_H 1/' > config.h.tmp && mv config.h.tmp config.h
   cat config.h | sed -e 's/.*undef HAVE_UNISTD_H.*/\#define HAVE_UNISTD_H 1/' > config.h.tmp && mv config.h.tmp config.h
   cat config.h | sed -e 's/.*undef HAVE_SYS_STAT_H.*/\#define HAVE_LIMITS_H 1/' > config.h.tmp && mv config.h.tmp config.h
-  if [ "$PLATFORM" = "x86_64-pc-cygwin" ]; then
+  if [ "$PLATFORM" = "x86_64-unknown-cygwin" ]; then
     echo "#undef HAVE_GETTIMEOFDAY" >> config.h
   fi
 
@@ -63,7 +71,7 @@ if [ $RETVAL != 0 ]; then
     echo "#define HAVE_SYS_STAT_H 1" >> config.h
   fi
   cd ../../
-  make
+  make -j$JOBS
 
   RETVAL=$?
   if [ $RETVAL != 0 ]; then
